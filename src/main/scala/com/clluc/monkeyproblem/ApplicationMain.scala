1e9eb447f67edd5893effd9dd0a585588985f487
Bernardo Uriarte <berublan@gmail.com>
2016-06-23 19:01:13 +0200

    Implement the program using the defined actors

--- src/main/scala/com/clluc/monkeyproblem/ApplicationMain.scala
+++ src/main/scala/com/clluc/monkeyproblem/ApplicationMain.scala
@@ -1,7 +1,34 @@
 package com.clluc.monkeyproblem
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Props}
+import scala.io.StdIn.readLine
+import scala.concurrent.duration._
+import scala.util.Random
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 object ApplicationMain extends App {
-  val system = ActorSystem("MyActorSystem")
+  def timeToNextRnd = (1 + Random.nextInt(8)).seconds
+
+  def dirRnd = if (Random.nextBoolean()) East else West
+
+  override def main(args: Array[String]) {
+    val system = ActorSystem("MyActorSystem")
+    val rope = system.actorOf(Props[Rope])
+
+    var monkeyId = 0
+
+    def nextMonkeyRunnable: Runnable = new Runnable {
+      override def run(): Unit = {
+        monkeyId += 1
+        system.actorOf(Monkey.props(monkeyId, dirRnd, rope))
+        system.scheduler.scheduleOnce(timeToNextRnd, nextMonkeyRunnable)
+      }
+    }
+    nextMonkeyRunnable.run()
+
+    println("press enter to end program")
+    readLine
+    system.terminate()
+  }
 }
