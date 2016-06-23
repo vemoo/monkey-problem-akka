13afaec491c3bbed3bbe78a5d05a236fb3b5746a
Bernardo Uriarte <berublan@gmail.com>
2016-06-23 21:06:03 +0200

    Create actor for monkey creation, because using system inside a runnable could fail when terminating

---
+++ src/main/scala/com/clluc/monkeyproblem/MonkeyGenerator.scala
@@ -0,0 +1,31 @@
+package com.clluc.monkeyproblem
+
+import akka.actor.{Actor, ActorRef, Props}
+import com.clluc.monkeyproblem.MonkeyGenerator.GenerateMonkeys
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.util.Random
+import scala.concurrent.duration._
+
+object MonkeyGenerator {
+
+  case object GenerateMonkeys
+
+  def props(rope: ActorRef): Props = Props(new MonkeyGenerator(rope))
+}
+
+class MonkeyGenerator(val rope: ActorRef) extends Actor {
+
+  def timeToNextRnd = (1 + Random.nextInt(8)).seconds
+
+  def dirRnd = if (Random.nextBoolean()) East else West
+
+  var monkeyId = 0
+
+  override def receive: Receive = {
+    case GenerateMonkeys =>
+      monkeyId += 1
+      context.system.actorOf(Monkey.props(monkeyId, dirRnd, rope))
+      context.system.scheduler.scheduleOnce(timeToNextRnd, self, GenerateMonkeys)
+  }
+}
